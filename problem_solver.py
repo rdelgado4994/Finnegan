f01013883673dd2d38c404d30e36ecba9b3a1330
Ronald Delgado <ronalddelgado@Ronalds-iMac.local>
2018-09-21 16:45:59 -0400

    update problem Solver

--- problem_solver.py
+++ problem_solver.py
@@ -9,4 +9,16 @@ class ProblemSolver
     
     def solve_harder_problem
     end
+
+    def sovle_tough_problem
+    end
+   
+    def solve_hill_cliemb_problem
+        hill_climb=HillClimb.new
+        hill_climb.climb
+    end
+  
+    def use_genetic_algorithm(problem)
+        darwin = Darwin.new(problem)
+    end
 end
